2c3ae677dc1c52356b21d34c1c4fd8643344f76c
Jari Mesina <jarimesina1234@gmail.com>
2020-12-06 23:13:09 +0800

    added features for resetting and removing cards

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
-import Card from './components/Card';
-import { randomNumberGenerator, findNextColor } from './helpers';
-import { colors } from './constants';
-import { Col, Row, Container } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import Card from "./components/Card";
+import { randomNumberGenerator, findNextColor } from "./helpers";
+import { colors } from "./constants";
+import { Col, Row, Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export class App extends Component {
   constructor() {
     super();
-    //colors are red,blue,green,orange,black,purple,yellow,pink
     this.state = {
-      cardData: []
-    }
-
+      cardData: [],
+      flipCount: 0,
+      score: 0,
+      firstPick: {},
+      secondPick: {},
+    };
   }
 
   async componentDidMount() {
@@ -21,23 +23,99 @@ export class App extends Component {
     for (let i = 0; i < 16; i++) {
       let pickedColor = randomNumberGenerator();
       if (pickedColor && colorsPicked[pickedColor] < 2) {
-        await this.setState({ cardData: [...this.state.cardData, { [`card_${i}`]: { color: colors[pickedColor].color, isFlipped: false } }] })
+        await this.setState({
+          cardData: [
+            ...this.state.cardData,
+            {
+              [`card_${i}`]: {
+                color: colors[pickedColor].color,
+                isFlipped: false,
+                isRemoved: false,
+              },
+            },
+          ],
+        });
         colorsPicked[pickedColor] = colorsPicked[pickedColor] + 1;
       } else {
         let newFoundColor = findNextColor(colorsPicked);
         colorsPicked[newFoundColor] = colorsPicked[newFoundColor] + 1;
-        await this.setState({ cardData: [...this.state.cardData, { [`card_${i}`]: { color: colors[newFoundColor].color, isFlipped: false } }] })
+        await this.setState({
+          cardData: [
+            ...this.state.cardData,
+            {
+              [`card_${i}`]: {
+                color: colors[newFoundColor].color,
+                isFlipped: false,
+                isRemoved: false,
+              },
+            },
+          ],
+        });
       }
-
     }
-
   }
 
-  handleClick = (key) => {
+  resetCards = () => {
+    const duplicate = this.state.cardData;
+
+    duplicate[this.state.firstPick.key][
+      `card_${this.state.firstPick.key}`
+    ].isFlipped = false;
+    duplicate[this.state.secondPick.key][
+      `card_${this.state.secondPick.key}`
+    ].isFlipped = false;
+
+    this.setState({ cardData: duplicate });
+  };
+
+  removeCards = () => {
+    const duplicate = this.state.cardData;
+
+    duplicate[this.state.firstPick.key][
+      `card_${this.state.firstPick.key}`
+    ].isRemoved = true;
+    duplicate[this.state.secondPick.key][
+      `card_${this.state.secondPick.key}`
+    ].isRemoved = true;
+
+    this.setState({ cardData: duplicate });
+  };
+
+  handleClick = async (key) => {
     const duplicate = this.state.cardData;
     duplicate[key][`card_${key}`].isFlipped = true;
-    this.setState({ cardData: [...duplicate ]});
-  }
+
+    //get cards that are flipped recently and compare
+    await this.setState({
+      cardData: [...duplicate],
+      flipCount: this.state.flipCount + 1,
+    });
+
+    if (this.state.flipCount == 1) {
+      await this.setState({
+        firstPick: { color: duplicate[key][`card_${key}`].color, key },
+      });
+    } else if (this.state.flipCount == 2) {
+      await this.setState({
+        secondPick: { color: duplicate[key][`card_${key}`].color, key },
+        flipCount: 0
+      });
+
+      if (this.state.firstPick.color === this.state.secondPick.color) {
+        this.setState({ score: this.state.score + 5 });
+
+        //a function for setting isRemoved to true
+        this.removeCards();
+      } else {
+        setTimeout(()=>{
+          this.resetCards();
+        }, 1000);
+        this.setState({ score: this.state.score - 1 });
+      }
+    }
+
+    await this.setState({ cardData: [...duplicate] });
+  };
 
   render() {
     const { cardData } = this.state;
@@ -48,19 +126,36 @@ export class App extends Component {
         <h1>Color Memory</h1>
         <Container>
           <Row>
-            {
-              cardData ? cardData.map((card, key) => {
-                return (
-                  <Col xs={3} lg={3} xl={3}><Card color={typeof card[`card_${key}`] !== 'undefined' ? card[`card_${key}`].color : "black"} status={card.status} onClick={this.handleClick} cardName={key} isFlipped={typeof card[`card_${key}`] !== 'undefined' ? card[`card_${key}`].isFlipped : false}/></Col>
-                )
-              }) : null
-            }
+            {cardData
+              ? cardData.map((card, key) => {
+                  return (
+                    //this is too long, fix me
+                    <Col xs={3} lg={3} xl={3}>
+                      <Card
+                        color={
+                          typeof card[`card_${key}`] !== "undefined"
+                            ? card[`card_${key}`].color
+                            : "black"
+                        }
+                        status={card.status}
+                        onClick={this.handleClick}
+                        cardName={key}
+                        isFlipped={
+                          typeof card[`card_${key}`] !== "undefined"
+                            ? card[`card_${key}`].isFlipped
+                            : false
+                        }
+                        isRemoved={card[`card_${key}`].isRemoved}
+                      />
+                    </Col>
+                  );
+                })
+              : null}
           </Row>
         </Container>
-
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react'
-import '../css/card.css';
+import React, { Component } from "react";
+import "../css/card.css";
 
 export class Card extends Component {
-    render() {
-        const {color} = this.props;
+  computeColor = () => {
+      if(this.props.isFlipped) {
+          if (this.props.isRemoved) {
+            return "white";
+          }
+          return this.props.color;
+      }
+      else {
+          return "black";
+      }
+  };
 
-        return (
-            <div className={`rectangle ${this.props.isFlipped ? color : "black"}`} onClick={() => this.props.onClick(this.props.cardName)} />
-        )
-    }
+  render() {
+    const { color } = this.props;
+
+    return (
+      <div
+        className={`rectangle ${this.computeColor()}`}
+        onClick={() => this.props.onClick(this.props.cardName)}
+      />
+    );
+  }
 }
 
-export default Card
+export default Card;
